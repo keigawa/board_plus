5df19ef8f413ca7164877e4094d8e51820556c9b
keigawa <kei.ogawa@finc.com>
2018-08-21 18:16:56 +0900

    pretend final

--- app/controllers/my_threads/comments_controller.rb
+++ app/controllers/my_threads/comments_controller.rb
@@ -1,9 +1,11 @@
 class MyThreads::CommentsController < ApplicationController
   before_action :set_my_thread 
   before_action :set_comment, only: [:edit, :update, :destroy] 
+  before_action :set_user_id_authentic, only: [:update, :destroy]
 
   def index
     @comments=Comment.where(my_thread_id:params[:my_thread_id])
+    @current_user_id=current_user.id
   end
 
   def new
@@ -11,7 +13,7 @@ class MyThreads::CommentsController < ApplicationController
   end
 
   def create
-    @my_thread.comments.create(comment_params)
+    @my_thread.comments.create(comment_params.merge(user_id: current_user.id))
     redirect_to my_thread_comments_path(@my_thread)
   end
 
@@ -19,12 +21,16 @@ class MyThreads::CommentsController < ApplicationController
   end
 
   def update
+    if @user_id_authentic==current_user.id
     @comment.update(comment_params)
+    end
     redirect_to my_thread_comments_path(my_thread_id: @my_thread)
   end
 
   def destroy
+    if @user_id_authentic==current_user.id
     @comment.destroy
+    end
     redirect_to my_thread_comments_path(my_thread_id: @my_thread)
   end
 
@@ -35,6 +41,10 @@ class MyThreads::CommentsController < ApplicationController
 
   def set_comment
     @comment=Comment.find(params[:id])
+  end
+  
+  def set_user_id_authentic
+    @user_id_authentic=@comment.user_id
   end
 
   def comment_params
